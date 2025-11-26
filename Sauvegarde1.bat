ff772fc74ef74b899fad9098b1400f875b9ffa27
Mestre <raphael.mestre18@gmail.com>
2025-11-26 09:31:22 +0100

    ajout de la sauvegarde.txt

--- Sauvegarde1.bat
+++ Sauvegarde1.bat
@@ -1,6 +1,6 @@
-cd Documents\xampp\htdocs\techsolutions\BDD
+cd c:\Documents\xampp\htdocs\techsolutions\BDD
 c:\Documents\xampp\mysql\bin\mysqldump -uroot techsolutions>techsolutions.sql
-cd Documents\xampp\htdocs\techsolutions
+cd c:\Documents\xampp\htdocs\techsolutions
 git add .
 git commit -m "ajout de la sauvegarde.txt"
 git push origin main 
